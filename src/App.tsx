143b39336e3a1feeaef4352752939c91c995f454
Geoff Johnson <geoff.jay@gmail.com>
2023-08-25 12:48:23 -0700

    ci: add dprint workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import { invoke } from "@tauri-apps/api/tauri";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
